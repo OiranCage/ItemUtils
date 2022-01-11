74b8f63f421eb94a6091582f72080bebde66c006
famima65536 <famima65536@gmail.com>
2022-01-11 16:44:45 +0900

    now accept integer as a parameter to duplicate item with specified quantity.

--- src/command/DuplicateCommand.php
+++ src/command/DuplicateCommand.php
@@ -2,6 +2,7 @@
 
 namespace oirancage\itemutils\command;
 
+use CortexPE\Commando\args\IntegerArgument;
 use CortexPE\Commando\args\TextArgument;
 use CortexPE\Commando\BaseSubCommand;
 use pocketmine\command\CommandSender;
@@ -11,6 +12,7 @@ class DuplicateCommand extends BaseSubCommand {
 
 	protected function prepare(): void{
 		$this->setPermission("itemutils.command.duplicate");
+		$this->registerArgument(0, new IntegerArgument("quantity", true));
 	}
 
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void{
@@ -19,13 +21,28 @@ class DuplicateCommand extends BaseSubCommand {
 			return;
 		}
 
+		$quantity = ($args["quantity"] ?? 1);
 		$inventory = $sender->getInventory();
-		$item = $inventory->getItemInHand();
-		if($inventory->canAddItem($item)){
-			$inventory->addItem($item);
-			$sender->sendMessage("Successfully duplicate item.");
-		}else{
-			$sender->sendMessage("You have no space to add item.");
+		$item = clone $inventory->getItemInHand();
+		$item->setCount($quantity);
+		$addableQuantity = $inventory->getAddableItemQuantity($item);
+		if($quantity <= 0){
+			$sender->sendMessage("Quantity should be bigger than 0.");
+			return;
 		}
+
+		if($addableQuantity === 0){
+			$sender->sendMessage("You have no space to add item.");
+			return;
+		}
+
+		if($addableQuantity < $quantity){
+			$item->setCount($addableQuantity);
+			$sender->sendMessage("You have not enough space to add $quantity items.");
+			$sender->sendMessage("Quantity has been changed to $addableQuantity.");
+		}
+
+		$inventory->addItem($item);
+		$sender->sendMessage("Successfully duplicate item.");
 	}
 }
